c054ae70754d2b72b95f9d0b81291dab86cbf9ae
LitvinovKV <SquertelSquertel@mail.ru>
2019-08-27 16:24:59 +0300

    Added readme disc nuget packages and comment for DataBaseContext class

--- Application/Models/Auth/DatabaseContext.cs
+++ Application/Models/Auth/DatabaseContext.cs
@@ -2,6 +2,7 @@
 using FluentNHibernate.Cfg.Db;
 using Microsoft.AspNet.Identity;
 using NHibernate;
+using NHibernate.Tool.hbm2ddl;
 using System.Configuration;
 
 namespace Application.Models.Auth
@@ -12,10 +13,16 @@ namespace Application.Models.Auth
 
         public DatabaseContext()
         {
+            // Получить строку подклчюения к БД из файла web.config
             string connectionString = ConfigurationManager.ConnectionStrings["connectionDB"].ConnectionString;
             this.sessionFactory = Fluently.Configure()
+                // Настройка подключения к БД
                 .Database(MsSqlConfiguration.MsSql2008.ConnectionString(connectionString))
+                // Настройка маппинга. Из-за AddFromAssemblyOf NHibernate будет пытаться 
+                // маппить каждый класс в этой сброке (assembly)
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<DatabaseContext>())
+                // ShemaUpdate позволяет создавать/обновлять таблицы и поля в БД
+                .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
                 .BuildSessionFactory();
         }
 
